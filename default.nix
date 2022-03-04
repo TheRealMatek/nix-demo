1bfe9d213e8cdb6f0efbf141c1149edf50a3d2c9
TheRealMatek <44670778+TheRealMatek@users.noreply.github.com>
2022-03-04 18:26:09 +0100

    Remove custom chord package

--- default.nix
+++ default.nix
@@ -7,7 +7,6 @@ with pkgs;
 let
   packages = rec {
     chord = callPackage ./pkgs/chord {};
-    chord_custom_sg = callPackage ./pkgs/chord { simgrid = custom_simgrid; };
     custom_simgrid = callPackage ./pkgs/simgrid/custom.nix {};
 
     inherit pkgs; # similar to `pkgs = pkgs;` This lets callers use the nixpkgs version defined in this file.
